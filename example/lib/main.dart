973b8f8dc3d3cb1fe5cd40a628fe063d04ab39aa
stormyang <stormyang@mail.com>
2024-04-02 13:12:12 +0800

    chore: add android example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:ve_game_view/ve_game_view.dart';
+
+import 'config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,35 +15,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _veGameViewPlugin = VeGameView();
+  late VeGameViewController gameViewController;
+
+  void _initAndStart() {
+    VeGameInitializer.init(accountId);
+  }
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _veGameViewPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -55,7 +34,25 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: VeGameView(
+              onCreated: (controller) {
+                gameViewController = controller;
+                controller.start(
+                  VeGameConfig(
+                    uid: uid,
+                    ak: ak,
+                    sk: sk,
+                    token: token,
+                    gameId: gameId,
+                    reservedId: "7352865170319809299",
+                    sessionMode: 2,
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
